ada17286ec73d0b4921c129ca8c114f19fba1ab8
Yusoo Hwang <yhwang20@jh.edu>
2022-10-10 19:29:30 -0700

    Changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/hw3-meme-gen-vanilla/Meme!/",
+  base: "/hw3-meme-gen-vanilla/",
   plugins: [react()]
 });
